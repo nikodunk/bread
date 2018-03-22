de39f449b4cf6ee3b6eac24af8a4325a8b577299
Nikolai <n.dunkel@gmail.com>
2018-03-22 03:14:02 -0700

    done for day 1

--- src/components/breadAdvertisement.js
+++ src/components/breadAdvertisement.js
@@ -2,11 +2,10 @@ import React from 'react';
 
 
  const Advertisement = () => (
-      <div className='container'>
-        <h2>Advertisement</h2>
-        <br />
-        <p></p>
-      </div>
+    <div>
+        <img src={process.env.PUBLIC_URL + '/WinterCampaign.jpg'} class="moneyShot" alt="" />
+        <br /><br /><br /><br />
+    </div>
     );
 
 export default Advertisement
