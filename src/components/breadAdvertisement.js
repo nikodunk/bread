85bbe34fe90c1d024bb6c4c980b818673c993b1e
Nikolai <n.dunkel@gmail.com>
2018-03-21 21:02:16 -0700

    bread react complete

---
+++ src/components/breadAdvertisement.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+ const Advertisement = () => (
+      <div className='advertisement'>
+        <h1>Advertisement</h1>
+        <p></p>
+      </div>
+    );
+
+export default Advertisement
---
+++ src/components/breadMusic.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+
+const Music = () => (
+      <div className='music'>
+        <h1>Music</h1>
+        <p></p>
+      </div>
+    );
+
+export default Music
