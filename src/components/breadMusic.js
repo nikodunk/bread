bd595db9c2491c699afa127f4731706d2de70f52
Nikolai <n.dunkel@gmail.com>
2018-03-22 15:46:22 -0700

    demo music added

--- src/components/breadMusic.js
+++ src/components/breadMusic.js
@@ -5,7 +5,7 @@ const Music = () => (
       <div className='container'>
         <h1>Music</h1>
         <br />
-        <p></p>
+        <iframe width="560" height="315" src="https://www.youtube.com/embed/NWypH92vMEw" frameborder="0" allow="autoplay; encrypted-media" allowfullscreen></iframe>
       </div>
     );
 
